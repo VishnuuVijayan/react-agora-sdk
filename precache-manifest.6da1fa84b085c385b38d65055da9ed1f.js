dcf107128b8dbbc484f2bc7a9cff807fcb14744a
VishnuuVijayan <vishnu08ptb@gmail.com>
2020-04-21 14:59:19 +0530

    Updates

---
+++ precache-manifest.6da1fa84b085c385b38d65055da9ed1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e7565bb804d7855eb7f997029f4720",
+    "url": "/react-agora-sdk/index.html"
+  },
+  {
+    "revision": "7dfa4f9f7374622afeca",
+    "url": "/react-agora-sdk/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4295630fd9fbed47110f",
+    "url": "/react-agora-sdk/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7dfa4f9f7374622afeca",
+    "url": "/react-agora-sdk/static/js/2.e0559242.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-agora-sdk/static/js/2.e0559242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4295630fd9fbed47110f",
+    "url": "/react-agora-sdk/static/js/main.fc09e08a.chunk.js"
+  },
+  {
+    "revision": "fa317302fbe8d0dfb48b",
+    "url": "/react-agora-sdk/static/js/runtime-main.aae51372.js"
+  }
+]);
